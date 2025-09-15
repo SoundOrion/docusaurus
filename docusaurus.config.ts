3520f344c12d23f1b2a37adbfd41cd715d1b05c1
hatsuyama <hatsuyama>
2025-09-15 21:27:08 +0900

    edit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://soundorion.github.io/docusaurus',
+  url: 'https://witty-ground-042f65900.1.azurestaticapps.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
