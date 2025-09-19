ec3975dfaecbd70da3ee5e67069552668b8e2b22
hatsuyama <hatsuyama>
2025-09-20 08:19:20 +0900

    add

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,7 +26,7 @@ const config: Config = {
   url: 'https://soundorion.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus/',
   deploymentBranch: 'gh-pages',
 
   organizationName: ORG_NAME,
